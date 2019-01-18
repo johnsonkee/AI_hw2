ff4db478bf5ff7b23635044cfabea52710c4da89
wxz <876688461@qq.com>
2019-01-18 16:01:42 +0800

    j

--- run.py
+++ run.py
@@ -13,17 +13,34 @@ import time
 import gluonbook as gb
 
 BATCH_SIZE = 128
-NUMS_EPOCHS = 50
+NUMS_EPOCHS = 2
 LR = 0.1
 USE_CUDA = True
+WD = 5e-4
+LR_PERIOD = 80
+LR_DECAY = 0.1
+MODEL_PATH = 'resnet10.params'
+USE_MODEL = False
+SAVE_MODEL = True
 
-def train(net, train_dataloader, test_dataloader,  batch_size, nums_epochs, lr, ctx):
-    trainer = Trainer(net.collect_params(), 'adam',
-                      {'learning_rate': lr})
+def train(net,
+          train_dataloader,
+          test_dataloader,
+          batch_size,
+          nums_epochs,
+          lr,
+          ctx,
+          wd,
+          lr_period,
+          lr_decay):
+    trainer = Trainer(net.collect_params(), 'sgd',
+                      {'learning_rate': lr, 'momentum':0.9, 'wd': wd})
     myloss = loss.SoftmaxCrossEntropyLoss()
     for epoch in range(nums_epochs):
         train_loss, train_acc, start = 0.0, 0.0, time.time()
-        for X,y in train_dataloader:
+        if epoch > 0 and epoch % lr_period == 0:
+            trainer.set_learning_rate(trainer.learning_rate * lr_decay)
+        for X, y in train_dataloader:
             # 原先的数据都是int8类型的，现在将其转化为float32
             # 以便输入到网络里面
             y = y.astype('float32').as_in_context(ctx)
@@ -44,33 +61,53 @@ def train(net, train_dataloader, test_dataloader,  batch_size, nums_epochs, lr, 
                       test_acc))
         print(epoch_s + time_s + ', lr' + str(trainer.learning_rate))
 
-def evaluate(net, test_dataloaders, ctx):
+def evaluate(net, test_dataloader, ctx):
     print(gb.evaluate_accuracy(test_dataloader, net, ctx))
 
-
-if __name__ == "__main__":
+def main():
     if USE_CUDA:
         ctx = gpu()
     else:
         ctx = cpu()
-    transform_format = transforms.Compose([
+    transform_train = transforms.Compose([
+        transforms.Resize(40),
+        transforms.RandomResizedCrop(32,scale=(0.64,1.0),
+                                     ratio=(1.0, 1.0)),
+        transforms.RandomFlipLeftRight(),
         transforms.ToTensor(),
         transforms.Normalize([0.4914, 0.4822, 0.4465],
                              [0.2023, 0.1994, 0.2010])
     ])
-    train_dataloader = DataLoader(_train_data.transform_first(transform_format),
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.4914, 0.4822, 0.4465],
+                             [0.2023, 0.1994, 0.2010])
+    ])
+    train_dataloader = DataLoader(_train_data.transform_first(transform_train),
                                   batch_size=BATCH_SIZE,
                                   shuffle=True,
                                   last_batch='keep')
 
-    test_dataloader = DataLoader(_test_data.transform_first(transform_format),
+    test_dataloader = DataLoader(_test_data.transform_first(transform_test),
                                  batch_size=BATCH_SIZE,
                                  shuffle=True,
                                  last_batch='keep')
 
     net = resnet18(num_classes=10)
-    net.initialize(ctx=ctx, init=init.Xavier())
-    print("====>train")
-    train(net, train_dataloader, test_dataloader, BATCH_SIZE, NUMS_EPOCHS, LR,ctx)
-    print("====>evaluate")
-    evaluate(net, test_dataloader, ctx)
+    net.hybridize()
+
+    if USE_MODEL:
+        net.load_parameters(MODEL_PATH)
+    else:
+        net.initialize(ctx=ctx, init=init.Xavier())
+
+    print("====>train and test")
+    train(net, train_dataloader, test_dataloader,
+          BATCH_SIZE, NUMS_EPOCHS, LR, ctx, WD,LR_PERIOD, LR_DECAY)
+    if SAVE_MODEL:
+        net.save_parameters(MODEL_PATH)
+
+
+if __name__ == "__main__":
+    main()
+
